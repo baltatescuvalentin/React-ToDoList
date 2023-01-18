4a55ee2b93d9e4e64514371a9b41f2c8ed05a265
baltatescuvalentin <baltatescuvalentin14@gmail.com>
2023-01-18 20:18:52 +0200

    .

--- src/components/TasksComponents/Sidebar.js
+++ src/components/TasksComponents/Sidebar.js
@@ -1,9 +1,12 @@
-
-// [#f5f5f54d]
+import { IoMdDoneAll } from 'react-icons/io';
+import { TbCalendarEvent, TbCalendarTime } from 'react-icons/tb';
+import { CgNotes } from 'react-icons/cg';
+import { BsPlusCircleFill } from 'react-icons/bs';
+import { ImFolderOpen } from 'react-icons/im';
 
 function Sidebar() {
     return (
-        <div className="flex flex-col bg-[#f5f5f54d] min-h-full w-[350px] shadow-[4px_0_5px_-2px_rgba(0,_0,_0,_0.7)]">
+        <div className="flex flex-col py-4 px-6 bg-[#f5f5f54d] min-h-full w-[350px] shadow-[4px_0_5px_-2px_rgba(0,_0,_0,_0.7)]">
             test
         </div>
     )
